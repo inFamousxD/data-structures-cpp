c5a79686fab8cb6b8cd3608ca8bdd5546d44a01c
inFamousxD <audimankar@gmail.com>
2021-01-03 02:36:46 +0530

    Created LinkedList of Linked Lists with continue

--- Data Structures/LinkedList.cpp
+++ Data Structures/LinkedList.cpp
@@ -4,13 +4,13 @@ using namespace std;
 // ============================================ Node Classes =================================== //
 class SinglyNode {
     public:
-        int data;
+        char data[100];
         SinglyNode* next;
 };
 
 class DoublyNode {
     public:
-        int data;
+        char data[100];
         DoublyNode* prev;
         DoublyNode* next;
 };
@@ -22,6 +22,7 @@ class SinglyLinkedList {
         int menuOption;
         int size;
     public:
+        void create();
         void initialize();
         void print();
         void addNode();
@@ -31,13 +32,14 @@ class SinglyLinkedList {
         void bulkAddNodes();
 };
 
-void SinglyLinkedList :: initialize() {
+void SinglyLinkedList :: create() {
     head = new SinglyNode();
     head->next = NULL;
-
     size = 0;
-
     cout << "Linked list Initialized.\n";
+}
+
+void SinglyLinkedList :: initialize() {
     do {
         cout << "\nChoose an option : \n\n1. Add a node.\n2. Delete a node (value).\n3. Print the entire list.\n";
         cout << "4. Add a node after (value).\n5. Add a node at (index).\n6. Bulk add nodes.\n99. Exit\n\n>> ";
@@ -61,6 +63,8 @@ void SinglyLinkedList :: initialize() {
             case 6:
                 bulkAddNodes();
                 break;
+            case 99:
+                break;
             default:
                 cout << "Please choose a valid option. \n>>";
                 break;
@@ -71,12 +75,12 @@ void SinglyLinkedList :: initialize() {
 void SinglyLinkedList :: print() {
     SinglyNode* temp;
     temp = head;
+    int n = 1;
 
     while (temp->next != NULL) {
         temp = temp->next;
-        cout << temp->data << " ";
+        cout << n++ << ". " << temp->data << "\n";
     }
-    cout << endl;
 }
 
 void SinglyLinkedList :: addNode() {
@@ -97,7 +101,7 @@ void SinglyLinkedList :: addNode() {
 }
 
 void SinglyLinkedList :: deleteNode() {
-    int value;
+    string value;
     cout << "Enter the value to be deleted.\n>> ";
     cin >> value;
 
@@ -131,7 +135,7 @@ void SinglyLinkedList :: deleteNode() {
 }
 
 void SinglyLinkedList :: addNodeAfterValue() {
-    int value;
+    string value;
     cout << "\nEnter the value after which node shall be added.\n>> ";
     cin >> value;
 
@@ -196,6 +200,140 @@ void SinglyLinkedList :: bulkAddNodes() {
     for (int i = 0; i < count; ++i) addNode();
 }
 
+// ==================================== Single Headed List Holder class ======================== //
+class SinglyListNode {
+    public:
+        string name;
+        SinglyLinkedList node;
+        SinglyListNode* next;
+        SinglyListNode* prev;
+};
+
+class SinglyListHolder {
+    private:
+        SinglyListNode* head;
+        int menuOption;
+        int size;
+    public:
+        void create();
+        void initialize();
+        void addList();
+        void continueEditingList();
+        void print();
+        void printAllLists();
+};
+
+void SinglyListHolder :: create() {
+    head = new SinglyListNode();
+    head->next = NULL;
+    head->prev = NULL;
+} 
+
+void SinglyListHolder :: initialize() {
+    cout << "\nChoose an option: ";
+    
+    do {
+        cout << "\n1. Add a new single headed list.";
+        cout << "\n2. Delete a list.";
+        cout << "\n3. Continue editing a list.";
+        cout << "\n4. Print all list names.";
+        cout << "\n5. Print all lists.";
+        cout << "\n99. Exit.\n\n>> ";
+        cin >> menuOption;
+
+        switch(menuOption) {
+            case 1:
+                addList();
+                break;
+            case 3:
+                continueEditingList();
+            case 4:
+                print();
+                break;
+            case 5:
+                printAllLists();
+                break;
+            case 99:
+                break;
+            default:
+                cout << "Please choose a valid option. \n>>";
+                break;
+        }
+    } while(menuOption != 99);
+}
+
+void SinglyListHolder :: addList() {
+    SinglyListNode* temp;
+    temp = head;
+
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    SinglyListNode* newNode;
+    newNode = new SinglyListNode();
+    cout << "\nName this list.\n>> ";
+    cin >> newNode->name;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    temp->next = newNode;
+
+    size++;
+
+    newNode->node.create();
+    newNode->node.initialize();
+}
+
+void SinglyListHolder :: print() {
+    SinglyListNode* temp;
+    temp = head;
+    int n = 1;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        cout << n++ << "." << temp->name << "\n";
+    }
+
+    cout << endl;
+}
+
+void SinglyListHolder :: continueEditingList() {
+    string name;
+
+    cout << "\nType the list name you want to continue from.\nSaved lists: \n";
+    print();
+    cout << ">> ";
+    cin >> name;
+
+    SinglyListNode* temp;
+    temp = head;
+
+    bool found = false;
+    while (true) {
+        if (temp->name == name) {
+            found = true;
+            break;
+        } else temp = temp->next;
+    }
+
+    if (!found) {
+        cout << "\nRecheck the name and try again!";
+        return;
+    } else {
+        temp->node.initialize();
+    }
+}
+
+void SinglyListHolder :: printAllLists() {
+    SinglyListNode* temp;
+    temp = head;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        cout << "\nPrinting : " << temp->name << endl;
+        temp->node.print();
+    }
+}
+
 // =================================================== Main ===================================== //
 int main() {
     cin.tie(0);
@@ -209,8 +347,9 @@ int main() {
         cin >> menuOption;
         switch(menuOption) {
             case 1:
-                SinglyLinkedList list;
-                list.initialize();
+                SinglyListHolder object;
+                object.create();
+                object.initialize();
                 break;
             case 2:
                 break;
